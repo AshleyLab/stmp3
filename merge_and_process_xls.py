66f7e01c570142d6f7c8ad1574e358476b04896c
Noah Daniel Friedman <noahfrie@sherlock-ln03.stanford.edu>
2017-10-24 13:52:05 -0700

    changes for merging

--- merge_and_process_xls.py
+++ merge_and_process_xls.py
@@ -28,6 +28,8 @@ def merge_and_add_columns(df1, df2, userColumnsToAdd, idxForColMappingsdf1, idxF
 		df1key = str(row[columnMappings['CHROM'][idxForColMappingsdf1]]) + ':' + str(row[columnMappings['POS'][idxForColMappingsdf1]])
 		for idx, r in df2.iterrows():
 			df2Key = str(r[columnMappings['CHROM'][idxForColMappingsdf2]]) + ':' + str(r[columnMappings['POS'][idxForColMappingsdf2]])
+			#print df2Key
+			#print '__________'
 			if df1key == df2Key:
 				#copy over values from pipeline df
 				for column in df1.columns.tolist():
@@ -48,13 +50,16 @@ def convert_to_float_or_zero(i):
 def add_allele_freq_summary_column(df):
 	alleleFreqCols = ['AF_EAS', 'AF_NFE', 'AF_SAS', 'AF_AMR', 'AF_AFR']
 	df['GNOMAD_Max_Allele_Freq'] = np.empty(len(df.index))
+	df['GNOMAD_Max_Allele_Freq_POP'] = np.empty(len(df.index))
+	df['GNOMAD_Max_Allele_Freq_POP'] = df['GNOMAD_Max_Allele_Freq_POP'].astype(str) #this will be a column of strings
 	for index, row in df.iterrows():
 		freqs = [convert_to_float_or_zero(row[i]) for i in alleleFreqCols]
-		population = alleleFreqCols[freqs.index(max(freqs))]
+		population = alleleFreqCols[freqs.index(max(freqs))].strip('AF_')
 		freq = max(freqs)
 		#ALERT todo: include the population from which the max freq comes
 		#df.set_value(index, 'Max_Allele_Freq', str(population) + ':' + str(freq))
 		df.set_value(index, 'GNOMAD_Max_Allele_Freq', freq)
+		df.set_value(index, 'GNOMAD_Max_Allele_Freq_POP', population)
 	#print df
 
 def fix_ref_or_alt_column_for_indels(df, refKey, altKey):
@@ -82,20 +87,59 @@ def sort_sheets(df):
 	sys.exit()
 	print df.sort([''])
 
+#arbitrary function for fixing issues with STMP/ingenuity inconsistency
+#basically if there is a stmp pos that is close to or equal to ingenuity pos overwrite the ingenuity pos with the stmp pos
+def find_closest_match_for_pos(stmpDf, ingenuityChrom, ingenuityPos):
+	minDistance = 100000 #default distance is arbitrary big number
+	newChrom, newPos, newRef, newAlt = -1, -1, -1, -1
+	for idx, r in stmpDf.iterrows():
+		stmpChrom, stmpPos = str(r['CHROM']), str(r['POS'])
+		stmpRef, stmpAlt = str(r['REF']), str(r['ALT'])
+		if stmpChrom == ingenuityChrom:
+			distance = abs(int(stmpPos) - int(ingenuityPos))
+			if distance < minDistance:
+				minDistance = distance
+				newChrom = stmpChrom
+				newPos = stmpPos
+				newRef = stmpRef
+				newAlt = stmpAlt
+	return newChrom, newPos, newRef, newAlt
+
+#in order to properly merge with the ingenuity XLS, we need to right align our STMP output
+#not generic function iterates over both dfs
+def realign_ingenuity_sheet(ingenuityDf, stmpDf):
+	for index, row in ingenuityDf.iterrows():
+		ingenuityChrom, ingenuityPos, ingenuityRef, ingenuityAlt = str(row['Chromosome']), str(row['Position']), str(row['Reference Allele']), str(row['Sample Allele'])
+		newChrom, newPos, newRef, newAlt = find_closest_match_for_pos(stmpDf, ingenuityChrom, ingenuityPos)
+		if newChrom > 0: #if we found a match please set it up
+			ingenuityDf.set_value(index, 'Chromosome', newChrom)
+			ingenuityDf.set_value(index, 'Position', newPos)
+			ingenuityDf.set_value(index, 'Reference Allele', newRef)
+			ingenuityDf.set_value(index, 'Sample Allele', newAlt)
+	return ingenuityDf
+
 def merge_columns_across_spreadsheets(spreadSheetPipeline, spreadSheetUser, outputDir, udnId):
 	sheetDictPipeline, sheetDictPipelineNames = read_xls_sheets(spreadSheetPipeline)
 	sheetDictUser, sheetDictUserNames = read_xls_sheets(spreadSheetUser)
 
-	fix_ref_or_alt_column_for_indels(sheetDictUser[sheetDictUserNames[0]], 'Reference Allele', 'Sample Allele') #this code fixes the syntax of indels for the user inputed sheet
-	fix_ref_or_alt_column_for_indels(sheetDictPipeline[sheetDictPipelineNames[1]], 'REF', 'ALT') #this code fixes the syntax of indels for the pipeline inputed sheet
+	#fix_ref_or_alt_column_for_indels(sheetDictUser[sheetDictUserNames[0]], 'Reference Allele', 'Sample Allele') #this code fixes the syntax of indels for the user inputed sheet
+	#fix_ref_or_alt_column_for_indels(sheetDictPipeline[sheetDictPipelineNames[1]], 'REF', 'ALT') #this code fixes the syntax of indels for the pipeline inputed sheet
+	sheetDictUser[sheetDictUserNames[0]] = realign_ingenuity_sheet(sheetDictUser[sheetDictUserNames[0]], sheetDictPipeline[sheetDictPipelineNames[1]])
+
 	#We expect the user's xls to be just a single sheet output from ingenuity.  If its not, that breaks our code and we exit
 	if len(sheetDictUser) != 1:
 		print 'error we expect an excel sheet from the user with a single sheet'
 		sys.exit()
 	mergedDfPipeline = merge_and_add_columns(sheetDictPipeline[sheetDictPipelineNames[1]], sheetDictUser[sheetDictUserNames[0]], [ #indicies indicate where we can find the two actual data spreadsheets
 	'Transcript ID', 'Transcript Variant', 'Protein Variant', 'Gene Region', 'Gene Symbol'], 0, 1) #list of columns to add from the user uploaded columns
-	mergedDfUser = merge_and_add_columns(sheetDictUser[sheetDictUserNames[0]], sheetDictPipeline[sheetDictPipelineNames[1]], [ #indicies indicate where we can find the two actual data spreadsheets
-	'AF_EAS', 'AF_NFE', 'AF_SAS', 'AF_AMR', 'AF_AFR', 'NC', 'NI', 'NA', 'ESP_AF_POPMAX', 'KG_AF_POPMAX', 'SD', 'SF', 'QUAL', 'ID', 'FILTER', 'GT', 'NJ', 'SX', 'GI'], 1, 0) # the 0s /1s relate to is it the GC xls first or the pipeline xls first
+	
+	colsToAddToDf = ['AF_EAS', 'AF_NFE', 'AF_SAS', 'AF_AMR', 'AF_AFR', 'NC', 'NI', 'NA', 'ESP_AF_POPMAX', 'KG_AF_POPMAX', 'SD', 'SF', 'QUAL', 'ID', 'FILTER', 'GT', 'NJ', 'SX', 'GI', 'AN_AFR', 'AN_AMR', 'AN_ASJ', 'AN_EAS', 'AN_FIN', 'AN_NFE', 'AN_OTH', 'AN_SAS']
+	colsFinal = []
+	for x in colsToAddToDf: #fix the columns in case they are missing
+		if x in sheetDictPipeline[sheetDictPipelineNames[1]].columns.tolist():
+			colsFinal.append(x)
+
+	mergedDfUser = merge_and_add_columns(sheetDictUser[sheetDictUserNames[0]], sheetDictPipeline[sheetDictPipelineNames[1]], colsFinal, 1, 0) # the 0s /1s relate to is it the GC xls first or the pipeline xls first
 	add_allele_freq_summary_column(mergedDfUser)
 
 	if False:
@@ -113,7 +157,7 @@ def merge_columns_across_spreadsheets(spreadSheetPipeline, spreadSheetUser, outp
 	return outputXlsxName
 
 
-renameDict = {'SX': 'SwissProtExpression', 'GI': 'ProximalGeneInfo', 'SD': 'SwissProtDiseaseAssociation', 'NJ': 'MutationTaster', 'SF': 'SwissProtFunction', 'NA': 'Phylop', 'NI': 'MutationTasterPVal', 'NC': 'Sift', 'GNOMAD_Max_Allele_Freq': 'GNOMADMaxAlleleFreq'}
+renameDict = {'SX': 'SwissProtExpression', 'GI': 'ProximalGeneInfo', 'SD': 'SwissProtDiseaseAssociation', 'mTaster': 'MutationTaster', 'SF': 'SwissProtFunction', 'phylop': 'Phylop', 'NI': 'MutationTasterPVal', 'sift': 'Sift', 'GNOMAD_Max_Allele_Freq': 'GNOMADMaxAlleleFreq'}
 #utility function we use to make the columns of the xls human readable as needed
 def make_cols_human_readable(df): 
 	colsToRename = renameDict
@@ -122,6 +166,32 @@ def make_cols_human_readable(df):
 		if key not in df.columns.tolist():
 			del colsToRename[key]
 	df = df.rename(columns=colsToRename)
+	return df
+
+#check each cell.  If the cell has a comma and the value before the comma is a float, just take the first half
+def clean_up_comma_separated_values(df):
+	cols = df.columns
+	for index, row in df.iterrows():
+		for col in cols:
+			commaIdx = str(row[col]).find(',')
+			if commaIdx > 0:
+				val = row[col][:commaIdx]
+				try:
+					float(val)
+					df.set_value(index, col, val)
+				except:
+					pass
+	return df
+
+def clean_cell_values(df):
+	df = clean_up_comma_separated_values(df)
+	valsToMarkAsEmpty = ['no value', '.']
+	emptyValue = ''
+	for col in df.columns:
+		if col == 'CADD Score':
+			df[col] = df[col].replace(['< 10'], 1)  #clean CADD score
+		df[col] = df[col].replace(valsToMarkAsEmpty, emptyValue) #normalize empty columns 
+		#clean up commas separated values
 	return df
 
 orderedCols = ['Chromosome', 'Position', 'Reference Allele', 'Sample Allele', 'Variation Type', 'Gene Region', 
@@ -150,6 +220,7 @@ def improve_legibility_of_xls(xlsName):
 	#to imporve legibility we do two things: make columns readable and sort columns
 	df = make_cols_human_readable(df)
 	df = sort_cols(df)
+	df = clean_cell_values(df)
 	outputXlsxName = xlsName
 	writer = pd.ExcelWriter(outputXlsxName,options={'encoding':'latin-1'})
 	df.to_excel(writer,'Sheet1', index = False)
