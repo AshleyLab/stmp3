a1241b178a7756fe59da2a3da8d7d7dd4b7d8223
Noah Daniel Friedman <noahfrie@sherlock-ln03.stanford.edu>
2017-10-24 13:52:22 -0700

    changes to allow for varsome api lookups

--- annotate_from_web_searches.py
+++ annotate_from_web_searches.py
@@ -7,14 +7,29 @@ import numpy as np
 import urllib
 import requests
 import re
+#alert please change!!!
+sys.path.append('/home/noahfrie/noahfrie/devCode/stmp3/variant-api-client-python/') #the varsome api code
+from variantapi.client import VariantAPIClient
+api_key = 'pC4Kh2WCojBfs!*9SY?XcwXYdE6T*4870eZQvsXR' #THIS KEY EXPIRES ON NOVEMEBER 6th!
 
 bingWebSearchKey ='960301165233425c8cb9bdd8d1d718a4' #note this is a free trial key and will eventually expire or be exhausted
 
 #mapping the column names to the function that fills them
+'an_amr'
+'an_afr'
+'an_asj'
+'an_eas'
+'an_fin'
+'an_nfe'
+'an_oth'
+'af_sas'
 searchFunctions = {		
  	'omim': lambda row: find_omim_link(row),	
  	'rvis': lambda row: find_rvis_value(row),
  	'fathmm': lambda row: parse_varsome_for_col(row, 'fathmm_score'),
+ 	'mTaster': lambda row: parse_varsome_for_col(row, 'mutationtaster_converted_rankscore'),
+ 	'sift': lambda row: parse_varsome_for_col(row, 'sift_converted_rankscore'),
+ 	'phylop': lambda row: parse_varsome_for_col(row, 'phylop100way_vertebrate_rankscore'),
  	'mgi': lambda row: parse_mgi(row)
  }
 
@@ -66,21 +81,39 @@ def find_rvis_value(row):
 	rvisScore = find_and_clean_regex(firstHalfRegexPattern, actualValueRegexPattern, secondHalfRegexPattern, html)
 	return rvisScore
 
+#corrects ref/alt for varsomes way of dealing with indels
+#based on the assumption of left alignment
+def correct_ref_alt_pos_for_varsome_indels(ref, alt, pos):
+	if len(ref) > len(alt):
+		newRef = ref[len(newRef): ]
+		dif = len(ref) - len(newRef)
+		position = int(pos) + dif #if ref if longer than alt we are basically deleting the front
+	else:
+		return 0
+
+
 #gets a single column value from varsome (called multiple times)
+'gnomad_exomes'
 def parse_varsome_for_col(row, field):
+	api = VariantAPIClient(api_key)
 	#get the key for the varsome search
 	chromosome = safely_convert_val_to_str(row['Chromosome'])
 	position = safely_convert_val_to_str(row['Position'])
 	ref = safely_convert_val_to_str(row['Reference Allele'])
 	alt = safely_convert_val_to_str(row['Sample Allele'])
-	prefixVarsomeUrl = 'https://api.varsome.com/lookup/'
-	suffixVarsomeUrl = '?add-all-data=1'
-	varsomeUrl = prefixVarsomeUrl + ':'.join([chromosome,position,ref,alt]) + suffixVarsomeUrl #properly format the url
-	response = requests.get(varsomeUrl)
+	#if len(ref) != len(alt): position, ref, alt = correct_ref_alt_pos_for_varsome_indels(ref, alt, position)
+	varsomeLookup = 'chr' + '-'.join([chromosome, position, ref, alt])
 	val = ''
 	try:
-		jsonResponse = response.json()
-		val = jsonResponse['dbnsfp'][0][field][0]
+		result = api.lookup(varsomeLookup, params={'add-source-databases': 'all'}, ref_genome='hg19')
+		try:
+			#gnomad data is formatted differently than dbnsfp data
+			if 'an_' in field:
+				val = result['gnomad_exomes'][0][field]
+			else:
+				val = result['dbnsfp'][0][field][0]
+		except:
+			pass
 	except:
 		pass
 	return val
@@ -112,10 +145,14 @@ def add_cols_for_df(df, searchKeys):
 #use this as a global variable so we dont have to do edge cases for how functions from our dictionary are set up
 #based on an input list of search keys we create a 
 def annotate_from_searches(searchKeys, xlsName):
+	#alwaysAnnotateFields = ['an_amr','an_afr','an_asj','an_eas','an_fin','an_nfe','an_oth','an_sas'] #things we always take from varsome
+	alwaysAnnotateFields = []
 	xls = pd.ExcelFile(xlsName)
 	df = xls.parse(xls.sheet_names[0]) #this is idx 0 because our merged xls files will always have their first sheet be a key for values
 	add_cols_for_df(df, searchKeys)
+	cntr = 0
 	for index, row in df.iterrows():
+		print cntr
 		#first iterate over all entries in the df
 		#then iterate over all search functions that we are on
 		for key, function in searchFunctions.items():
@@ -123,6 +160,13 @@ def annotate_from_searches(searchKeys, xlsName):
 				function = searchFunctions[key] 
 				value = function(row)
 				df.set_value(index, key, value)
+		#now do the annotations we always do
+		for field in alwaysAnnotateFields:
+			value = parse_varsome_for_col(row, field)
+			if not str(value).isdigit(): value = float('nan')  #set the value to be nan if there is no value
+			df.set_value(index, field, value)
+
+		cntr += 1
 
 	#write the df and return it
 	outputXlsxName = xlsName
